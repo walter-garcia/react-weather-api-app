026755ef49bc3ccc68d80f57c63044b1e48517a9
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-25 21:38:53 -0300

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
 class App extends Component {
   render() {
     return (
       <>
-        <div>Hello React!</div>
+        <div>
+        <Titles />
+        <Form />
+        <Weather />
+        </div>
       </>
     )
   }
