366bbaa57d79557fd725805e0095e1503f6a8b58
Walter Marcel Barbosa Garcia <waltermbgarcia@hotmail.com>
2019-09-25 22:34:42 -0300

    update components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,24 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
+const api_key = "250102fb638d0a11ebcdf69e573d1361";
+
 class App extends Component {
+  getWeather = async (event) => {
+    event.preventDefault();
+    const api_call = await 
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=Natal,BR&appid=${api_key}&units=metric`)
+    ;
+
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <>
         <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
         </div>
       </>
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,11 @@ class Form extends Component {
   render() {
     return (
       <>
-        <div>
-          Form Component
-        </div>
+        <form onSubmit={this.props.getWeather}>
+          <input type="text" name="city" placeholder="City..." />
+          <input type="text" name="country" placeholder="Country..." />
+          <button>Get Weather</button>
+        </form>
       </>
     );
   }
